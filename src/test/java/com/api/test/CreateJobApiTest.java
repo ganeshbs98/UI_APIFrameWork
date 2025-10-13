bc6415a37f57fe37c0e04d75717d707e22c4eaa4
fh-ganeshbs <ganesh.bs@foodhub.com>
2025-10-13 20:28:15 +0530

    modified the Test files

--- src/test/java/com/api/test/CreateJobApiTest.java
+++ src/test/java/com/api/test/CreateJobApiTest.java
@@ -8,6 +8,8 @@ import com.api.pojo.*;
 import com.api.utils.AuthTokenProvider;
 import com.api.utils.ConfigManager;
 import static com.api.utils.DateTimeUtil.*;
+
+import com.api.utils.DateTimeUtil;
 import com.api.utils.specUtil;
 import io.restassured.http.ContentType;
 
@@ -15,6 +17,7 @@ import static io.restassured.module.jsv.JsonSchemaValidator.*;
 
 import static org.hamcrest.Matchers.*;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.time.Instant;
@@ -22,15 +25,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CreateJobApiTest {
+   private CreateJobPayload createJobPayload;
+
+    @BeforeMethod(description = "creating the creat job api payload")
+    public void setUp(){
+        Customer customer = new Customer("Denis", "Boyer", "986-350-8890", "9823984974", "abc@gmail.com", "abc2@gmail.com");
+        System.out.println(customer.first_name());
+        Customer_Address customer_address = new Customer_Address("c 304", "Jupiter", "MG road", "Bangur Nagar", "Goregaon West", "411039", "India", "Maharashtra");
+        Customer_Product customerProduct = new Customer_Product(getDateTimeWithAgo(10), "26889419449447", "26889419449447", "26889419449447", getDateTimeWithAgo(10), Product.NEXUS_2.getCode(), Model.NEXUS_2_BLUE.getCode());
+        Problems problems = new Problems(Problem.SMARTPHONE_IS_RUNNING_SLOW.getCode(), "Battery Issue");
+        List<Problems> problemsList = new ArrayList<>();
+        problemsList.add(problems);
+        createJobPayload = new CreateJobPayload(Service_Location.SERVICE_LOCATION_A.getCode(), Platform.FRONT_DESK.getCode(), Warrenty_Status.IN_WARRANTY.getCode(), OEM.GOOGLE.getCode(), customer, customer_address, customerProduct, problemsList);
+    }
+
+
 
     @Test(description ="Verifying the create Job api is able to create the inwarrenty job" ,groups = {"api","regression","smoke"})
 
     public void createJobApiTest() {
-        System.out.println(Instant.now());
-        System.out.println("***********************");
-
-
-
 
         given().spec(specUtil.RequestSpec_withHeader_Payload(FD, createJobPayload)).when().post("/job/create").then().spec(specUtil.ResponseSpec_Ok()).body(matchesJsonSchemaInClasspath("response-schema/CreateJobApiResponseSchema.json")).body("message", equalTo("Job created successfully. ")).body("data.mst_service_location_id", equalTo(1)).body("data.job_number", startsWith("JOB_"));
         // Test implementation will go here
--- src/test/java/com/api/test/UserDetailsApiTest.java
+++ src/test/java/com/api/test/UserDetailsApiTest.java
@@ -30,17 +30,7 @@ import java.util.List;
 public class UserDetailsApiTest {
     private CreateJobPayload createJobPayload;
 
-    @BeforeMethod(description = "creating the creat job api payload")
-    public void setUp(){
-        Customer customer = new Customer("Denis", "Boyer", "986-350-8890", "9823984974", "abc@gmail.com", "abc2@gmail.com");
-        System.out.println(customer.first_name());
-        Customer_Address customer_address = new Customer_Address("c 304", "Jupiter", "MG road", "Bangur Nagar", "Goregaon West", "411039", "India", "Maharashtra");
-        Customer_Product customerProduct = new Customer_Product(getDateTimeWithAgo(10), "26889419449447", "26889419449447", "26889419449447", getDateTimeWithAgo(10), Product.NEXUS_2.getCode(), Model.NEXUS_2_BLUE.getCode());
-        Problems problems = new Problems(Problem.SMARTPHONE_IS_RUNNING_SLOW.getCode(), "Battery Issue");
-        List<Problems> problemsList = new ArrayList<>();
-        problemsList.add(problems);
-        createJobPayload = new CreateJobPayload(Service_Location.SERVICE_LOCATION_A.getCode(), Platform.FRONT_DESK.getCode(), Warrenty_Status.IN_WARRANTY.getCode(), OEM.GOOGLE.getCode(), customer, customer_address, customerProduct, problemsList);
-    }
+
 
 
     @Test(description = "verfiy if the user details api response is  shown correctly",groups = {"api","regression","smoke"})
