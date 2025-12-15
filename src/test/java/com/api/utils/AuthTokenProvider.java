5c9bbf967ca46a4ee25bce3ae83fadedf5eff73f
fh-ganeshbs <ganesh.bs@foodhub.com>
2025-12-15 20:43:09 +0530

    added code for token caching for authtoken

--- src/test/java/com/api/utils/AuthTokenProvider.java
+++ src/test/java/com/api/utils/AuthTokenProvider.java
@@ -5,27 +5,38 @@ import static com.api.constants.Role.*;
 import com.api.constants.Role;
 import com.api.pojo.UserCredentials;
 import io.restassured.http.ContentType;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 import static org.hamcrest.Matchers.*;
 
 import static io.restassured.RestAssured.*;
 
 public class AuthTokenProvider {
-    public static String getAuthToken(Role role){
-        UserCredentials userCredentials=null;
-        if(role==FD){
-            userCredentials=new UserCredentials("iamfd", "password");
-        } else if (role==SUP) {
-            userCredentials=new UserCredentials("iamsup", "password");
+    private static Map<Role, String> tokenCache = new ConcurrentHashMap<Role,String>();
+    public static String getAuthToken(Role role) {
+        UserCredentials userCredentials = null;
+        if (tokenCache.containsKey(role)) {
+            return tokenCache.get(role);
+        }
+        if (role == FD) {
+            userCredentials = new UserCredentials("iamfd", "password");
+        } else if (role == SUP) {
+            userCredentials = new UserCredentials("iamsup", "password");
 
-        } else if (role==ENG) {
-            userCredentials=new UserCredentials("iameng", "password");
+        } else if (role == ENG) {
+            userCredentials = new UserCredentials("iameng", "password");
 
-        }else if(role==QC){
-            userCredentials=new UserCredentials("iamqc", "password");
+        } else if (role == QC) {
+            userCredentials = new UserCredentials("iamqc", "password");
 
         }
-        String token=given().baseUri(ConfigManager.loadProperties().getProperty("URI")).contentType(ContentType.JSON).body(userCredentials).when().post("login").then().log().ifValidationFails().body("message", equalTo("Success")).extract().body().jsonPath().getString("data.token");
-       return token;
+        String token = given().baseUri(ConfigManager.loadProperties().getProperty("URI")).contentType(ContentType.JSON).body(userCredentials).when().post("login").then().log().ifValidationFails().body("message", equalTo("Success")).extract().body().jsonPath().getString("data.token");
+        tokenCache.put(role, token);
+        return token;
 
     }
 }
